7fe78e2be7c6902a7f9bfc5da6c7a016f467adfc
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-01-26 13:16:18 -0700

    Added basic structure of MyGame class

---
+++ Assn1-GameLoop/GameLoop/GameLoop/MyGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLoop
+{
+    class MyGame
+    {
+        // Internal class to hold information about game events
+        class GameEvent
+        {
+            string Name { get; set; }
+            int Duration { get; set; }
+            int Count { get; set; }
+        }
+
+        // this list holds all game events
+        private List<GameEvent> GameEvents;
+
+        // the "constructor"
+        public void initialize()
+        {
+            Console.WriteLine("GameLoop Demo Initializing...");
+            GameEvents = new List<GameEvent>();
+        }
+
+        // where the actual game loop will take place
+        public void run()
+        {
+
+        }
+    }
+}
