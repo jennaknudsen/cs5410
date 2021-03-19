ab33964b3924b9cb8bcc2f954518d50a4a8b6dc9
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-03-18 18:47:21 -0600

    Added Particles subproject

---
+++ Subprojects/Particles/Particles/Game1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Particles.Particles;
+
+namespace Particles
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private ParticleEmitter _allEmitter;
+        private ParticleEmitter _angleEmitter;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _allEmitter = new ParticleEmitter(
+                Content,
+                new TimeSpan(0, 0, 0, 0, 25),
+                200,
+                200,
+                20,
+                1,
+                new TimeSpan(0, 0, 0, 6),
+                new TimeSpan(0, 0, 0, 3)
+            );
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            _allEmitter.update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.Additive);
+
+            _allEmitter.draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
