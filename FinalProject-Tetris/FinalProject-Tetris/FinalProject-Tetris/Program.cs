452b897cf285093b9d33f9287be16ac3f7f2fb86
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-03-28 20:46:42 -0600

    Added FinalProjct-Tetris/ project

---
+++ FinalProject-Tetris/FinalProject-Tetris/FinalProject-Tetris/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FinalProjecct_Tetris
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new TetrisGame())
+                game.Run();
+        }
+    }
+}
