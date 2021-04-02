d03c96e2d92d9d58eb6c817bcbccc7816ec68e52
Jonas Knudsen <jonas.a.knudsen@gmail.com>
2021-04-02 10:44:59 -0600

    Added four metrics functions

--- FinalProject-Tetris/FinalProject-Tetris/FinalProject-Tetris/AiController.cs
+++ FinalProject-Tetris/FinalProject-Tetris/FinalProject-Tetris/AiController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
 namespace FinalProject_Tetris
 {
     public class AiController
@@ -58,12 +61,60 @@ namespace FinalProject_Tetris
             return completeLineCount;
         }
 
+        // get total number of squares in holes
         private float Holes(Square[,] boardSquares)
         {
             var holesCount = 0;
 
+            for (var col = 0; col < 10; col++)
+            {
+                // counting gaps in this column; only apply them if we see a non-gap after we add a gap
+                var gapsInColumn = 0;
+                for (var row = 0; row < 20; row++)
+                {
+                    // if this is an empty square then increment gap count
+                    if (boardSquares[col, row] == null)
+                    {
+                        gapsInColumn++;
+                    }
+                    // otherwise, apply the gap count and reset gap count to 0
+                    else
+                    {
+                        holesCount += gapsInColumn;
+                        gapsInColumn = 0;
+                    }
+                }
+            }
+
             return holesCount;
         }
 
+        // get total bumpiness of board
+        private float Bumpiness(Square[,] boardSquares)
+        {
+            // get heights of each column
+            var heights = new int[10];
+
+            for (var col = 0; col < 10; col++)
+            {
+                for (var row = 19; row >= 0; row--)
+                {
+                    if (boardSquares[col, row] != null)
+                    {
+                        heights[col] = row + 1;
+                        break;
+                    }
+                }
+            }
+
+            // now, sum the absolute differences between all columns
+            var runningTotal = 0;
+            for (var i = 0; i < heights.Length - 1; i++)
+            {
+                runningTotal += Math.Abs(heights[i] - heights[i + 1]);
+            }
+
+            return runningTotal;
+        }
     }
 }
